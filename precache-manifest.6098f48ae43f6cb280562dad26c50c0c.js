754d35468780197731b4f0dfba3b7e90b463fbae
Dung <dung.dao19@outlook.com>
2019-10-16 19:31:16 -0500

    Updates

---
+++ precache-manifest.6098f48ae43f6cb280562dad26c50c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509f714b1f8aba18e8003d26b2cdcd46",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "cf6059777a616b02dffc",
+    "url": "/monsters_rolodex/static/css/main.33fff2f0.chunk.css"
+  },
+  {
+    "revision": "77dbf4f8356f8673966b",
+    "url": "/monsters_rolodex/static/js/2.24f13ce1.chunk.js"
+  },
+  {
+    "revision": "cf6059777a616b02dffc",
+    "url": "/monsters_rolodex/static/js/main.3cacdb2c.chunk.js"
+  },
+  {
+    "revision": "3112d4dbdc6aff97a28a",
+    "url": "/monsters_rolodex/static/js/runtime-main.67d2ce3c.js"
+  },
+  {
+    "revision": "bedc2a4d4f9d2c6657cf5a743be4fdad",
+    "url": "/monsters_rolodex/static/media/shadow_isles.bedc2a4d.jpeg"
+  }
+]);
